319c3ce31ca621a6bcdbfb4be252a34314b707c3
KY_HA <ihs20191663@gmail.com>
2022-12-04 07:00:17 +0000

    v4_setup_stack()

--- userprog/process.c
+++ userprog/process.c
@@ -544,8 +544,15 @@ setup_stack (void **esp)
   if (kpage != NULL) 
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
-      if (success)
+      if (success){
         *esp = PHYS_BASE;
+        struct vm_entry *vme = malloc(sizeof(struct vm_entry));
+        vme->type = VM_ANON;
+        vme->vaddr = ((uint8_t *) PHYS_BASE) - PGSIZE;
+        vme->writable = true;
+        vme->is_loaded = true;
+        insert_vme(&thread_current()->vm, vme);
+      }
       else
         palloc_free_page (kpage);
     }
