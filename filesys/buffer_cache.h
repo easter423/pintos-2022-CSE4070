67e5df25ae5b9b75970f3f9a83147ff52191aaf2
KY_HA <ihs20191663@gmail.com>
2022-12-24 08:38:51 +0000

    v5_buffer_cache

---
+++ filesys/buffer_cache.h
@@ -0,0 +1,25 @@
+#ifndef FILESYS_BUFFER_CACHE_H
+#define FILESYS_BUFFER_CACHE_H
+
+#include <stdbool.h>
+#include "filesys/off_t.h"
+#include "devices/block.h"
+
+struct buffer_cache_entry{
+    bool valid_bit;
+    bool reference_bit;
+    bool dirty_bit;
+    block_sector_t disk_sector;
+    uint8_t buffer[BLOCK_SECTOR_SIZE];
+};
+
+void buffer_cache_init(void);
+void buffer_cache_terminate(void);
+void buffer_cache_read(block_sector_t, void*, off_t, int, int);
+void buffer_cache_write(block_sector_t, void*, off_t, int, int);
+struct buffer_cache_entry *buffer_cache_lookup(block_sector_t);
+struct buffer_cache_entry *buffer_cache_select_victim(void);
+void buffer_cache_flush_entry(struct buffer_cache_entry*);
+void buffer_cache_flush_all(void);
+
+#endif
